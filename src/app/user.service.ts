b1701076d16e05a2d35826b28808d0f88293fb5e
dakalo <dakalotshikwela@gmail.com>
2023-08-15 10:20:29 +0200

    Servive for users

---
+++ src/app/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import studentData from '../student.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  getUsers(){
+  return studentData.users;
+
+  }
+}
